3a1186359e7c5dc30c1294e0584b89bb83a19933
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-31 11:50:35 -0400

    yeasa

--- guess_flag/flag_game/views.py
+++ guess_flag/flag_game/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def home(request):
-    template = 'templates/'
-    return HttpResponse()
+    template = loader.get_template('home.html')
+    context = { }
+    
+    return HttpResponse(template.render(context, request))
